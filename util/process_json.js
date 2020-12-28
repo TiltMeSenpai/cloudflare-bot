9065653aa59cb3b611de472e04d28b31e7d6753b
Josh Koike <jk@tilting.me>
2020-12-27 17:36:14 -0800

    Why do windows v2

--- util/process_json.js
+++ util/process_json.js
@@ -8,7 +8,7 @@ const fetch = require('node-fetch');
     let jsdoc_path = "node_modules/.bin/jsdoc"
     if(process.platform == "win32")
         jsdoc_path = "node_modules/.bin/jsdoc.bat"
-    const {stdout} = await execFile(jsdoc_path, ["-X", "commands.js"])
+    const {stdout} = await execFile("node", [jsdoc_path, "-X", "commands.js"])
     const jsdoc = JSON.parse(stdout)
     let commands = []
     let choices = new Map()
